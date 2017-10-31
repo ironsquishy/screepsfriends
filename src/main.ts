84936ecca326869751b1591bc312574dc2aa1d29
ironsquishy <spacecore88@gmail.com>
2017-10-31 08:24:03 -0700

    Removed game.ts.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ Memory.builders = Memory.builders || [];
 Memory.upgraders = Memory.upgraders || [];
 Memory.explorers = Memory.explorers || [];
 
+Memory.maxHarvCount  = 3;
+Memory.maxBuildCount = 2;
+Memory.maxUpgradeCount = 2;
 
 import {gameManager} from "./game/game.manager";
 
